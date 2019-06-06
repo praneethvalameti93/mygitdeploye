bb58fd0f3be3cb10f3dfa4a846b63116ac73eda5
praneethvalameti93 <praneethvalameti93@gmail.com>
2019-06-06 19:49:49 +0530

    Updates

---
+++ precache-manifest.f4018cb740a30ef7d6a1d173cef5f260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729620088c6205577803a9942281a3a5",
+    "url": "/mygitdeploye/index.html"
+  },
+  {
+    "revision": "9ff9c4452acbd9988c01",
+    "url": "/mygitdeploye/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mygitdeploye/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9ff9c4452acbd9988c01",
+    "url": "/mygitdeploye/static/js/main.03f4890e.chunk.js"
+  },
+  {
+    "revision": "d00be65934858a95f3c1",
+    "url": "/mygitdeploye/static/js/runtime~main.adb2e1cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mygitdeploye/static/media/logo.5d5d9eef.svg"
+  }
+]);
